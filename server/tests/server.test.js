2c29cdb7a47eba1a7a15c8d8fdd5d2f0c8218ad8
timtnguyen <nguyen710150@yahoo.com>
2018-10-15 22:08:42 -0700

    Add test cases for GET /cards/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest'); 
+const { ObjectID } = require('mongodb'); 
 
 const { app } = require('../server'); 
 const { CreditCard } = require('../models/creditcard'); 
 
 let cards = [{
+    _id: new ObjectID(), 
     card: 'Citi Chase', 
     balance: 1200
 }, {
+    _id: new ObjectID(), 
     card: 'CapitalOne',
     balance: 2100
 }];
@@ -83,3 +86,33 @@ describe('GET /cards', () => {
             .end(done); 
         }); 
 });
+
+describe('GET /cards/:id', () => {
+    it('should return card doc', (done) => {
+        request(app)
+            .get(`/cards/${cards[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.card.card).toBe('Citi Chase');
+            })
+            .end(done); 
+    });
+
+    it('should return 404 if card not found', (done) => {
+        request(app)
+            .get('/cards/123')
+            .expect(404)
+            .end(done); 
+    });
+
+    it('should return 404', (done) => {
+        let hexID = new ObjectID().toHexString();
+        request(app)
+            .get(`/cards/${hexID}`)
+            .expect(404)
+            .end(done); 
+    });
+});
+
+
+
