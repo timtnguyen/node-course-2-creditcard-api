f28f86141a68c6325b72095d47a60af74707c44d
timtnguyen <nguyen710150@yahoo.com>
2018-10-28 21:37:52 -0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -85,6 +85,27 @@ userSchema.statics.findByToken = function(token) {
     });
 };
 
+userSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+
+    return User.findOne({ email })
+        .then((user) => {
+            if (!user) {
+                return Promise.reject(); 
+            }
+
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if (res) {
+                        resolve(user); 
+                    } else {
+                        reject(); 
+                    }
+                });
+            });
+        });
+};
+
 const validateUser = (user) => {
     const schema = {
         name: Joi.string().min(1).max(250).required(), 
--- server/server.js
+++ server/server.js
@@ -351,8 +351,7 @@ app.post('/users', (req, res) => {
 
     newUser.save()
         .then(() => {
-            return newUser.generateAuthToken(); 
-            //res.send(user); 
+            return newUser.generateAuthToken();
         }).then((token) => {
             res.header('x-auth', token).send(newUser);
         }).catch((err) => {
@@ -364,6 +363,21 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user); 
 });
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']); 
+    
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            return user.generateAuthToken()
+                .then((token) => {
+                    res.header('x-auth', token).send(user); 
+                });
+        }).catch((err) => {
+            res.status(400).send(); 
+        });
+}); 
 
 
 //https://git.heroku.com/polar-reef-69029.git
