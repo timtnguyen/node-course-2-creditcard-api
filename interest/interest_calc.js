bea8f4bbc6865d338beca69baba6534a7af8eea0
timtnguyen <nguyen710150@yahoo.com>
2018-11-18 23:29:55 -0800

    Add calculation method

--- interest/interest_calc.js
+++ interest/interest_calc.js
@@ -1,22 +1,83 @@
-// const { CreditCard } = require('../models/creditcard'); 
-// const { Expense, validateExpense } = require('../models/expense');
-// const { Payment, validatePayment } = require('../models/payment'); 
+const { CreditCard } = require('../models/creditcard'); 
+const { Expense, validateExpense } = require('../models/expense');
+const { Payment, validatePayment } = require('../models/payment'); 
+const schedule = require('node-schedule'); 
 
-// module.exports.calculateTax = () => {
-//     setInterval(() => {
-        
-//         CreditCard.update(
-//             { card: 'Discover'},
-//             { $mul: { balance: 1.000678 } }
-//         ).then((result) => {
-//             console.log(result); 
-//         });
+module.exports.calculateTax = () => {
+    let rule = new schedule.RecurrenceRule();
+    rule.hour = 1; 
 
-//         CreditCard.update(
-//             { card: 'Citi Costco' },
-//             { $mul: { balance: 1.000465 } }
-//         ).then((result) => {
-//             console.log(result); 
-//         });
-//     }, 1000000); 
-// }
+    schedule.scheduleJob(rule, function() {
+        console.log(rule);
+
+        CreditCard.update(
+            { card: 'Discover'},
+            { $mul: { balance: 1.0006298 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'American Express' },
+            { $mul: { balance: 1.0004106 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'Golden1' },
+            { $mul: { balance: 1.000301 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'BOA Cash Rewards' },
+            { $mul: { balance: 1.0005202 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'BOA Balance Rewards' },
+            { $mul: { balance: 1.0006572 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'Citi Preferred' },
+            { $mul: { balance: 1.0006846 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'Citi Simplicity' },
+            { $mul: { balance: 1.0006572 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+    
+        CreditCard.update(
+            { card: 'Citi Costco' },
+            { $mul: { balance: 1.0004723 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'Chase Freedom' },
+            { $mul: { balance: 1.0006846 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+
+        CreditCard.update(
+            { card: 'Chase Southwest' }, 
+            { $mul: { balance: 1.0004723 } }
+        ).then((result) => {
+            console.log(result); 
+        });
+    });   
+};
